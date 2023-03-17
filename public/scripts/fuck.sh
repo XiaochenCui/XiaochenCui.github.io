dfa90acc23ee95c8808e59cc906dbf04d7db9ed3
XiaochenCui <jcnlcxc.new@gmail.com>
2023-03-17 17:32:36 +0800

    update blog content

--- public/scripts/fuck.sh
+++ public/scripts/fuck.sh
@@ -5,5 +5,13 @@ set -o errexit
 set -o nounset
 
 SCRIPT_PATH="/tmp/fuck.py"
+
+# -f, --force: ignore nonexistent files and arguments, never prompt
+rm --force ${SCRIPT_PATH}
+
+# -O file
+# --output-document=file
+#   The documents will not be written to the appropriate files, but all will be concatenated together and written to file.
 wget --output-document=${SCRIPT_PATH} https://cuixiaochen.com/scripts/fuck.py
+
 python3 ${SCRIPT_PATH}
