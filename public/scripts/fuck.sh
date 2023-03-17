6882d01efd67efb999db5ff62ee03985d9fac5b4
XiaochenCui <jcnlcxc.new@gmail.com>
2023-03-17 17:28:26 +0800

    update blog content

--- public/scripts/fuck.sh
+++ public/scripts/fuck.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+set -o xtrace
+set -o errexit
+set -o nounset
+
 SCRIPT_PATH="/tmp/fuck.py"
 wget --output-document=${SCRIPT_PATH} https://cuixiaochen.com/scripts/fuck.py
 python3 ${SCRIPT_PATH}
